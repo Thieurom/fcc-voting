54d2ead427282570dd3a5734bdb1f1ec15945989
Doan Le Thieu <thieurom@gmail.com>
2016-12-13 22:10:01 +0700

    Setup templates, css; add main route for homepage

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 'use strict'
 
+// Deps
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const server = express();
 
-server.get('/', (req, res) => {
-  res.send('It works!');
-});
+// Routes
+const main = require('./routes/main');
 
-server.listen(PORT, () => {
+// App instance
+const app = express();
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', main);
+
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT} ...`);
 });
