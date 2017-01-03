656815899b2fdc43737074e8b49cc9294021a71a
Doan Le Thieu <thieurom@gmail.com>
2017-01-04 00:37:42 +0700

    Add fontawesome cdn

--- routes/logout.js
+++ routes/logout.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Protected route
 router.get('/', Auth.isLoggedIn, (req, res) => {
   req.logout();
-  req.flash('logoutMsg', 'You\'ve been logged out!');
+  req.flash('homeMsg', 'You\'ve been logged out!');
   res.redirect('/');
 });
 
