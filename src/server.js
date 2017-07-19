dac3e0ab4890e2405ecc85d79dfe75a4df785118
Doan Le Thieu <thieurom@gmail.com>
2017-07-19 22:26:13 +0700

    Add index page

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from './views/components/App';
 
 
 const app = express();
 
 app.use(morgan('dev'));
+
+// Server settings
+app.use(express.static(path.join(__dirname, 'assets')));
+app.set('views', path.join(__dirname, 'views/layouts'));
+app.set('view engine', 'pug');
+
+
 app.get('/', (req, res) => {
-    res.send('Welcome to Voting App!');
+    const title = 'Votee! Another voting app';
+    const content = ReactDOMServer.renderToString(<App />);
+    res.render('index', {title, content});
 });
 
 app.listen(3000, () => {
---
+++ src/views/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header(props) {
+    return (
+        <header className='header'>
+            <div className='header__inner'>
+                <a className='brand-logo' href='/'>Votee!</a>
+            </div>
+        </header>
+    );
+}
+
+
+export default Header;
